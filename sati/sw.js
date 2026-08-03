c06793f32cef71ee8560e7fad6353786be836b1b
aneesh-kuda <99090958+aneesh-kuda@users.noreply.github.com>
2026-08-02 17:21:25 -0700

    Add files via upload

--- sati/sw.js
+++ sati/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'sati-v5';
+const CACHE = 'sati-v6';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
